6ad84f9b31b03028f47f6de21b71fb419bc72fd7
SehaniSenevirathne <47855887+SehaniSenevirathne@users.noreply.github.com>
2022-09-18 21:49:56 +0530

    create login and home screen

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2G2EgSIFbvGi29YNs2-5bci9nSE0vhas",
+  authDomain: "fir-billmagic.firebaseapp.com",
+  projectId: "fir-billmagic",
+  storageBucket: "fir-billmagic.appspot.com",
+  messagingSenderId: "268743317748",
+  appId: "1:268743317748:web:f0f120cde40d47de8da6a1"
+};
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0) {
+    app = firebase.initializeApp(firebaseConfig);
+} else {
+    app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export {auth};
+ 
