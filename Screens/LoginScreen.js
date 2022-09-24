983bb4b126aa5cb375cfd4de36bb1de9f32db248
SehaniSenevirathne <47855887+SehaniSenevirathne@users.noreply.github.com>
2022-09-24 11:13:52 +0530

    changed styles

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,136 +1,136 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  View,
+  SafeAreaView,
+} from "react-native";
 import { auth } from "../firebase";
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const navigation = useNavigation()
+  // const navigation = useNavigation();
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            if(user){
-                NavigationPreloadManager.replace("Home")
-            }
-        })
-        return unsubscribe
-    }, [])
+  useEffect(() => {
+    // const unsubscribe = auth.onAuthStateChanged((user) => {
+    //   if (user) {
+    //     NavigationPreloadManager.replace("Home");
+    //   }
+    // });
+    // return unsubscribe;
+  }, []);
 
-    const handleSignUp = () => {
-        auth 
-        .createUserWithEmailAndPassword(email, password)
-        .then(userCredentials => {
-            const user = userCredentials.user;
-            console.log('Registered in with:', user.email);
-        })
-        .catch(error => alert(error.message))
-    }
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("Registered in with:", user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    const handleLogin = () => {
-        auth
-        .signInWithEmailAndPassword(email, password)
-        .then(userCredentials => {
-            const user = userCredentials.user;
-            console.log('Logged in with:', user.email);
-        })
-    }
+  const handleLogin = () => {
+    auth.signInWithEmailAndPassword(email, password).then((userCredentials) => {
+      const user = userCredentials.user;
+      console.log("Logged in with:", user.email);
+    });
+  };
 
-    return (
-         <KeyboardAvoidingView
-            styles={styles.container}
-            behavior="padding"
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.input}
+        />
+
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={handleLogin} style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}
         >
-         <View styles={styles.inputContainer}>
-            <TextInput
-            placeholder="Email"
-            value={email}
-            onChangeText={text => setEmail(text)}
-            style={styles.input}
-            />
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-            <TextInput
-            placeholder="Password"
-            value={password}
-            onChangeText={text => setPassword(text)}
-            style={styles.input}
-            secureTextEntry
-            />
-
-         </View>
-
-         <View style={styles.buttonContainer}>
-            <TouchableOpacity
-            onPress={handleLogin}
-            style={styles.button}
-            >
-                <Text style={styles.buttonText}>Login</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-            onPress={handleSignUp}
-            style={[styles.button, styles.buttonOutline]}
-            >
-                <Text style={styles.buttonOutlineText}>Register</Text>
-            </TouchableOpacity>
-            
-         </View>
-         </KeyboardAvoidingView>
-        )
-}
-
-export default LoginScreen
+export default LoginScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    marginHorizontal: 16,
+  },
 
-    inputContainer: {
-        width: '80%'
-    },
+  inputContainer: {
+    flex: 1,
+    // backgroundColor: "red",
+    justifyContent: "flex-end",
+  },
 
-    input: {
-        backgroundColor: 'white',
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginTop: 5,
-    },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
 
-    buttonContainer: {
-        width: '60%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 40,
-    },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    // backgroundColor: "green",
+    marginTop: 30,
+  },
 
-    button: {
-        backgroundColor: '#0782F9',
-        width: '100%',
-        padding: 15,
-        borderRadius: 10,
-        alignItems: 'center',
-    },
+  button: {
+    backgroundColor: "#0782F9",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
 
-    buttonOutline: {
-        backgroundColor: 'white',
-        marginTop: 5,
-        borderColor: '#0782F9',
-        borderWidth: 2,
-    },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#0782F9",
+    borderWidth: 2,
+  },
 
-    buttonText: {
-        color: 'white',
-        fontWeight: '700',
-        fontSize: 16,
-    },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
 
-    buttonOutlineText: {
-        color: '0782F9',
-        fontWeight: '700',
-        fontSize: 16,
-    },
-})
+  buttonOutlineText: {
+    color: "0782F9",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
